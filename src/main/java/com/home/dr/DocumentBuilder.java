b7c40c5a7156b272465b90b30baf314af5a6e00d
diman <diman@Dish.home>
2016-05-15 17:08:00 -0400

    added Document builder

---
+++ src/main/java/com/home/dr/DocumentBuilder.java
@@ -0,0 +1,28 @@
+package com.home.dr;
+
+import java.util.List;
+
+public class DocumentBuilder {
+	SentenceTokenizer sentenceTokenizer = new SentenceTokenizer();
+	
+	WordTokenizer wordTokenizer = new WordTokenizer();
+	
+	NamedEntityExtractor namedEntityExtractor;
+	
+	public DocumentBuilder() {}
+	
+	public Document buildDocument(String text, List<String> entities) {
+		
+		Document doc = new Document();
+		namedEntityExtractor = new NamedEntityExtractor(entities);
+		
+		doc.setSentences(sentenceTokenizer.getSentences(doc));
+		
+		for(Sentence s : doc.sentences) {
+			s.setWords(wordTokenizer.getWords(s));
+			s.setNamedEntities(namedEntityExtractor.getNamedEntities(s));
+		}
+		
+		return doc;
+	}
+}
