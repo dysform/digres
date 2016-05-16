b907b93ef1e8b68f8ee873f83c82dc1575949bf8
diman <diman@Dish.home>
2016-05-15 23:54:49 -0400

    fixed input/output dir issues

--- src/main/java/com/home/dr/DigitalReasoning.java
+++ src/main/java/com/home/dr/DigitalReasoning.java
@@ -6,39 +6,27 @@ import org.apache.commons.io.FileUtils;
 
 public class DigitalReasoning {
 	
+	String inputDir;
 	String outputDir;
 
-	public DigitalReasoning(String outputDir) {
+	public DigitalReasoning(String inputDir, String outputDir) {
+		this.inputDir = inputDir;
 		this.outputDir = outputDir;
 	}
 
 	public void run() throws Exception {
-		ClassLoader classLoader = getClass().getClassLoader();
-		
-		String inputFileName = classLoader.getResource("nlp_data.txt").toURI().getPath();
-		String nerFileName = classLoader.getResource("NER.txt").toURI().getPath();
+		String inputFileName = inputDir+"/nlp_data.txt";
+		String nerFileName = inputDir+"/NER.txt";
 		
 		Document doc = new DocumentBuilder().buildDocument(
 				FileUtils.readFileToString(new File(inputFileName)),
 				FileUtils.readLines(new File(nerFileName)));
-		
-		doc.printSentences();
-		
-		System.out.println("");
-		System.out.println("");
-		
-		doc.printWords();
-		
-		System.out.println("");
-		System.out.println("");
-		
-		doc.printNamedEntities();
-		
+
 		DocumentXML.printDocument(doc, outputDir+"/objectModel.xml");
 	}
 	
 	public static void main(String[] args) throws Exception {
-		DigitalReasoning dr = new DigitalReasoning(args[0]);
+		DigitalReasoning dr = new DigitalReasoning(args[0], args[1]);
 		dr.run();
 	}
 }
