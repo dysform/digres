dbb9029b23c9f86f3bc6fcd4526054b8d4f07e52
diman <diman@Dish.home>
2016-05-15 21:58:13 -0400

    added multithreading

--- src/main/java/com/home/dr/DocumentBuilder.java
+++ src/main/java/com/home/dr/DocumentBuilder.java
@@ -1,8 +1,11 @@
 package com.home.dr;
 
+import java.io.InputStream;
 import java.util.List;
 
-public class DocumentBuilder {
+import org.apache.commons.io.IOUtils;
+
+public class DocumentBuilder implements Runnable{
 	SentenceTokenizer sentenceTokenizer = new SentenceTokenizer();
 	
 	WordTokenizer wordTokenizer = new WordTokenizer();
@@ -10,6 +13,29 @@ public class DocumentBuilder {
 	NamedEntityExtractor namedEntityExtractor;
 	
 	public DocumentBuilder() {}
+	
+	InputStream in;
+	
+	List<String> entities;
+	
+	Document output;
+	
+	boolean error = false;
+	
+	public DocumentBuilder(InputStream in, List<String> entities) {
+		this.in = in;
+		this.entities = entities;
+	}
+	
+	public void run() {
+		try {
+			String text = IOUtils.toString(in);
+			output = buildDocument(text, entities);
+		}
+		catch(Exception e) {
+			error = true;
+		}
+	}
 	
 	public Document buildDocument(String text, List<String> entities) {
 		
--- src/main/java/com/home/dr/DocumentXML.java
+++ src/main/java/com/home/dr/DocumentXML.java
@@ -1,11 +1,13 @@
 package com.home.dr;
 
 import java.io.File;
+import java.util.List;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 
 public class DocumentXML {
+	
 	public static void printDocument(Document doc, String fileName) throws Exception {
 		File file = null;
 		if(fileName!=null) 
@@ -17,5 +19,18 @@ public class DocumentXML {
 		jaxbMarshaller.marshal(doc, System.out);
 		if(file!=null)
 			jaxbMarshaller.marshal(doc, file);
+	}
+	
+	public static void printDocuments(List<Document> docs, String fileName) throws Exception {
+		File file = null;
+		if(fileName!=null) 
+			file = new File(fileName);
+		JAXBContext jaxbContext = JAXBContext.newInstance(Document.class);
+		Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+		jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+		jaxbMarshaller.marshal(docs, System.out);
+		if(file!=null)
+			jaxbMarshaller.marshal(docs, file);
 	}
 }
